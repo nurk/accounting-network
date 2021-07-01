c82e4d189749650edfb20212f00f8f1c456e38e6
Tom Briers <tbriers@telenet.be>
2021-07-01 14:34:03 +0200

    Just some setup

---
+++ src/main/java/com/example/accountingnetwork/AccountingNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.accountingnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingNetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountingNetworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/accountingnetwork/AccountingNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accountingnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountingNetworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
